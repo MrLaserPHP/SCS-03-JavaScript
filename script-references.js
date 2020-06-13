505d32d6735b8d306a7757d1c3bd1bbb9c8d338d
Tony Facciolo <tony.facciolo@codingmetrology.com>
2020-06-13 01:46:29 -0400

    Initial commit of rough ovelay within JS. Not yet a functional set of content

---
+++ script-references.js
@@ -0,0 +1,85 @@
+
+// SYNTAX EXAMPLES ************************* SYNTAX EXAMPLES
+// 
+// var userName = prompt("What's your name?");
+// var confirmCats = confirm("Do you like cats?");
+// var petName = "Meeses";
+// var petType = "Tuxedo Cat";
+// var petAge = 5;
+// var petIsCat = true;
+
+//     if (confirmCats) {
+//         alert(petName + " " + petType + " " + petAge);
+//       }
+//     else {
+//         alert("You don't get my cat's information");
+//       }
+
+//     if (petIsCat) {
+//         alert("My pet is a cat");
+//       }
+//     else {
+//         alert("My pet is not a cat");
+//       }
+
+//     if (petAge !== 7) {
+//         alert("My pet is not 7 years old");
+//       }
+
+//     if (petAge === 5) {
+//         alert("My pet is 5 years old");
+//       }
+//     else if (petAge < 5) {
+//         alert("My pet is less than 5 years old");
+//       }
+//     else if (petAge > 5) {
+//         alert("My pet is older than 5 years old");
+//       }
+
+//       alert("Welcome to our page " + userName);
+
+//       var catAge = prompt("How old is your cat?");
+
+//       // Won't work as expected
+//       alert(catAge + 2);
+
+//       // Works as expected
+//       alert(parseInt(catAge) + 2);
+
+
+// alert("We definitely rock!");
+
+// // Notice that the confirm and prompt take in variables but the alert doesn't
+// var doYouRock = confirm("The question is, do *you* rock?");
+// var howMuchRock = prompt("How much do you rock?");
+
+// // Alert the results
+// alert(doYouRock);
+// alert(howMuchRock);
+
+
+    //   // Created an array named myBands.
+    //   var myBands = ["britney", "selena", "bob dylan", "blink 182", "michael jackson"];
+
+    //   // Created a prompt to get the user's favorite band.
+    //   var userGuess = prompt("Which band or artist do you like?");
+
+    //   // Converts the user's answer to lowercase.
+    //   var userGuessLower = userGuess.toLowerCase();
+
+    //   // Now we check the myBands array to see if it contains the user's answer.
+    //   // If the user's band is not in the array...
+    //   if (myBands.indexOf(userGuessLower) === -1) {
+    //     alert("Nah! They're pretty lame...");
+    //   }
+    //   // If it is in the array...
+    //   else {
+    //     alert("OMG! I love them too!");
+
+    // console.log("I love " + vegetables[i]);
+
+
+// THIS below breaks the code of the html page 
+// document.body.innerHTML = "This is some text";
+// alert(alphabet)
+// alert(Math.random())
---
+++ script.js
@@ -0,0 +1,79 @@
+// Declaring variables here
+// ****Is there a way to decalre a set of common characters without defining each and every one?
+// var alphabet = alphabet
+// var numbers = numbers 
+// var specialcharacters = specialcharacters
+
+// var alphabet = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+var alphabetlow = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabetup = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+// Need to update the special characters variable list to either utilise unicode or escape sequences in order to have all special characters utilised
+// var specialcharacters = [!#%&()*+,-./:;<=>?@[\]^{|}~];
+var uppercase = "A";
+// OR   
+var alphanumerics = 'abcdef123456!@#$%^';
+// var alphanumerics = 'abcdef123456!@#$%^' .split
+var poorpasword1 = alphabetlow + numbers;
+var poorpasword2 = alphabetlow + alphabetup + numbers + alphanumerics;
+var randomoption1 = (Math.floor((Math.random() * 3) + 1))
+// 
+// var generatedpassword = 
+
+// ********* This one below confuses me
+var generate = document.querySelector('#generate')
+var mytextareatest1 = document.querySelector('#password')
+// Variables declared
+
+// Event listers...
+generate.addEventListener("click", function() 
+{console.log('ACTIVATED Generate Button! Woohoo.') 
+//THIS ...'.value' is...
+//whatever comes after a '.' dot is an element. 
+//After parentheses are attributes. 
+mytextareatest1.value = "i'm changing this value to nullllllllllllll"
+}
+)
+
+
+// console trial
+console.log("var alphabetlo:");
+console.log(alphabetlow);
+console.log("var alphabetup:");
+console.log(alphabetup);
+console.log("numbers:");
+console.log(numbers);
+console.log("alphanumerics:");
+console.log(alphanumerics);
+console.log("poorpasword1:");
+console.log(poorpasword1);
+console.log("poorpasword2:");
+console.log(poorpasword2);
+console.log("randomoption1");
+console.log(randomoption1);
+console.log("Math.floor((Math.random() * 10) + 0):");
+console.log(Math.floor((Math.random() * 10) + 0));
+console.log("Math.floor((Math.random() * 26) + 0):");
+console.log(Math.floor((Math.random() * 26) + 0));
+console.log("Random Alphabet Index is " + "A");
+console.log( "Alphabet letter #4 is " + alphabetlow[4-1]);
+document.write("Example written text to html")
+// alert trial
+alert( "Alphabet letter #4 is " + alphabetlow[4-1]);
+
+// after or in the button area, run folloiwng line of code to prevent unwanted...
+// event.preventDefault();
+
+// STEPS *************** STEPS
+// first need to start a loop limitied to the length of the number of characters desired for password, perhaps 24?
+// for first character, need to use option var to decide which of the 3 types of characters to use. Otherwise need to concatinate all characters and choose randomly from thos?
+// then need to add a ++ to the loop
+// also need to re-randomise the option number variable.
+
+
+
+// function clearentry() {
+//   var x = document.getElementById("demo"); 
+// }
+
+
